ab94010c565fef2092cc867c6a1de497c9e26a6c
JuanSant <jsantiago@itexico.net>
2016-06-16 21:01:47 -0500

    1st Commit
    This contains separate scripts which represent each module from the project on the "Head First Object-Oriented..." book implemented through JavaScript. Moreover, the topics being covered on "YDKJS" book series as well as the "JS Design Patterns" are applied to the same code

---
+++ YDKJS and Head First Object-Oriented Analysis and Design/Guitar.js
@@ -0,0 +1,32 @@
+import guitarSpecAPI from GuitarSpec;
+
+var serialNumber, price, guitarSpec;
+
+function Guitar(serialNumber, price){
+	this.serialNumber = serialNumber;
+	this.price = price;
+}
+
+function getSerialNumber(){
+	return serialNumber;
+}
+
+function getPrice(){
+	return price;
+}
+
+function setPrice(newPrice){
+	this.price = newPrice;
+}
+
+function getGuitarSpec(){
+	return guitarSpecAPI;
+}
+
+var guitarAPI{
+	Guitar : Guitar,
+	getPrice : getPrice,
+	getGuitarSpec : getGuitarSpec
+}
+
+return guitarAPI;
