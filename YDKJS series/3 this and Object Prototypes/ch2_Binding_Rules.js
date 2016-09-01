1b16fe14b6034fff3c68471566bb8f933552dfff
JuanSant <jsantiago@itexico.net>
2016-09-01 17:15:12 -0500

    Chapter 2
    Binding Rules: Default, Implicit and Explicit

---
+++ YDKJS series/3 this and Object Prototypes/ch2_Binding_Rules.js
@@ -0,0 +1,37 @@
+/****Binding Rules****/
+/*There are 4 rules that "this" follows to decide what call-site should point to*/
+
+//--Default Building--//
+//This rule points to function call made in global scope
+function foo() {
+	console.log(this.a); //reference to global property
+}
+
+var a = 2;
+
+foo(); //prints 2
+
+//--Implicit Binding--//
+//Rule where call-site has a context object
+function bar() {
+	console.log( this.b ) //reference to object property
+}
+
+var obj = {
+	b : 10,
+	bar: bar
+};
+
+obj.bar(); //10
+
+//--Explicit Binding--//
+//Rule in which the function call is forced to use a specific object for the "this" binding
+function fn() {
+	console.log(this.c);
+}
+
+var obj = {
+	c : 4
+};
+
+fn.call( obj ); //4 -Call forcing "this" to be obj
