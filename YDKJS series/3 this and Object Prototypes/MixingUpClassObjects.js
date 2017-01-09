9fd2170e8a50106ebeb148403a6a165d93ce1dae
JuanSant <jsantiago@itexico.net>
2017-01-09 16:12:04 -0600

    Class Theory

---
+++ YDKJS series/3 this and Object Prototypes/MixingUpClassObjects.js
@@ -0,0 +1,3 @@
+//Classes describe a way for organizing the code in order to model it as a real world problem.
+//Thus, through data structures, classes help us to operate on data and package it up along with a certain behavior (a.k.a. Encapsulate).
+//It's possible to classify data structures by thinking about any given structure as a specific variation of a more general base definition.
