6a42669ae9352bb9c45656be96c1db4abf75def2
JuanSant <jsantiago@itexico.net>
2017-01-23 17:06:16 -0600

    JS Mixins

--- YDKJS series/3 this and Object Prototypes/ch4_MixingUpClassObjects.js
+++ YDKJS series/3 this and Object Prototypes/ch4_MixingUpClassObjects.js
@@ -21,3 +21,28 @@
 //Metaphorically, a class would be the equivalent to a Blue-print in terms of architecture, so in order to start using it
 //it's mandatory to build it (a.k.a. Instantiate it). Once built, the result of such "construction" is an object, so this object
 //acts like a copy with all the characteristics described by the class.
+
+//Constructor is a method, usually called same way as class, whose explicit job is 
+//to initialize any information (state) the instance will need.
+
+//in most of class-oriented languages, it's necessary to type "new" 
+//to let the language engine know you want to construct a new class instance
+
+//Class Inheritance//
+//As previously mentioned, it's possible to inherit from one class to another. The second class is named "child" whereas
+//the first class is called "parent". Thus, once the child exists, it's separate from the parent, 
+//so it only takes initally a copy of the behavior from the parent, but can override it and define new ones.
+
+//Polymorphism is considered relative, in contrast with other class-oriented languages, since works under the idea that 
+//any method cna reference another method at a higher level of the inheritance hierarchy. 
+//Moreover, a method name can have multiple definitions at different levels of the inheritance so they're selected as apropiate
+//when resolving which methods are being called.
+
+//In JS is more apropiate to think of the "class" belonging to the constructor (The Foo.prototype... type references). 
+//Since in JS the relationship between parent and child class exists only between the two ".prototype" objects of the
+//respective constructors, the constructors themselves are not directly related, 
+//and thus there's no simple way to relatively reference one from the other.
+//Class inheritance implies copies.
+
+//Mixins
+//JS doesn't count with "classes" to instantiate only objects, so they're not copied to others, they just get linked together.
