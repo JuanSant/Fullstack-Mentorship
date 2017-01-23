fa1f47ae8d327ccb9ad4dd912ebaad8fffba4e15
JuanSant <jsantiago@itexico.net>
2017-01-23 13:11:09 -0600

    Class Theory
    Polymorfism, and class explanation.

--- YDKJS series/3 this and Object Prototypes/ch4_MixingUpClassObjects.js
+++ YDKJS series/3 this and Object Prototypes/ch4_MixingUpClassObjects.js
@@ -1,3 +1,23 @@
 //Classes describe a way for organizing the code in order to model it as a real world problem.
 //Thus, through data structures, classes help us to operate on data and package it up along with a certain behavior (a.k.a. Encapsulate).
 //It's possible to classify data structures by thinking about any given structure as a specific variation of a more general base definition.
+ 
+//Example
+//We can define a general class such as "Person" and it'll have some properties and specific behavior.
+//However, this can be detailed in more specific classes or classifications like "Client" and "Employee". Thus,
+//although each of these classes are "Person" at some point, they have their own properties and behaviors besides
+//the ones from "Person" class. Saying that, classes, inheritance and instantiation arise.
+
+//Polymorfism is another important topic in classes, since by using it, it's possible to override parent methods to re-use them in
+//child classes, including more specific properties or other details that are not setup in parent class.
+
+//Nevertheless, JavaScript is not a ¨class¨ language, so it only provides class-like synctactic elements that lets 
+//you implementing approximations of classical class functionality.
+
+//as other class-orientes-languages, JS offers a Stack class which is a standard library that provides a "stack" data structure 
+//with some properties that store data and publicly accessible behaviors to interact with the (hidden) data. For using it
+//it's necessary to instantiate the Stack class.
+
+//Metaphorically, a class would be the equivalent to a Blue-print in terms of architecture, so in order to start using it
+//it's mandatory to build it (a.k.a. Instantiate it). Once built, the result of such "construction" is an object, so this object
+//acts like a copy with all the characteristics described by the class.
