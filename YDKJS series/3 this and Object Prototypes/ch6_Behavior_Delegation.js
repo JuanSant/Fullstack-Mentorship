d6c8465ccfff81753a374f4e3c560bb43584a63c
JuanSant <jsantiago@itexico.net>
2017-03-23 17:38:55 -0600

    Delegation behavior design

--- YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
+++ YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
@@ -1,1 +1,26 @@
+//What's important to the functionality we can leverage in JS, is
+//all about objects being linked to other objects.
+
+//-Delegation-Oriented design
+//Instead of solving problems by creating/simulating classes, Delegation points to
+//define first an object with concrete behavior, then another object to hold a
+//specific data/behavior, so you link the task-specific object(s) to the 1st object
+//allowing them to delegate to it when necessary.
+var Task = {
+    setID: function(ID) { this.id = ID; },
+    outputID : function() { console.log(this.id); }
+ };
+
+//this makes XYZ delegate to "Task"
+var XYZ = Object.create( Task );
+
+XYZ.prepateTask = function(ID, Label) {
+    this.setID(ID);
+    this.label = Label;
+};
+
+XYZ.outputTaskDetails = function(){
+    this.outputID();
+    console.log(this.label)
+};
 
