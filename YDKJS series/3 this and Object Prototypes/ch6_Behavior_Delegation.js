29c48b6421537b49815d8c3521e11766b62c3c61
JuanSant <jsantiago@itexico.net>
2017-03-30 17:54:07 -0600

    Delegating Widget Objects

--- YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
+++ YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
@@ -114,3 +114,56 @@ b2.speak();
 //linked to each other, without needing all the cruft and confusion of things that look like classes, with constructors
 //and prototypes and new calls.
 
+//OLOO-style code embraces the fact that the only thing we ever really care about is the objects linked to other objects.
+
+//oin ES6 is implemented the class syntax, however, despite syntactic improvements, these are not real classes,
+//as they still operate on top of the [[Prototype]] mechanism.
+
+//-Delegating Widget Objects--//
+var Widget = {
+    init: function(width, height){
+        this.width = width || 50;
+        this.height = height || 50;
+        this.$elem = null;
+    },
+    insert: function($where){
+                if(this.$elem){
+                    this.$elem.css({
+                        width: this.width + "px",
+                        height: this.height + "px"
+                        }).appendTo( $where );
+                }
+    }
+};
+
+var Button = Object.create( Widget );
+
+Button.setup = function(width, height, label){
+    //Delegated call
+    this.init(width, height);
+    this.label = label || "Default";
+    
+    this.$elem = $( "<button>" ).text( this.label );
+};
+
+Button.build = function($where){
+    //Delegated call
+    this.insert($where);
+    this.$elem.click( this.onClick.bind( this ) );
+};
+
+Button.onClick = function(evt){
+    console.log( "Button '" + this.label + "' clicked!" );
+};
+
+$( document ).ready( function(){
+        var $body = $( document.body );
+        var btn1 = Object.create( Button );
+        btn1.setup(125, 30, "hello");
+    
+        var btn2 = Object.create( Button );
+        btn2.setup(150, 40, "World!");
+    
+        btn1.build( $body );
+        btn2.build( $body );
+});
