290e3b9d5ce331ad8aafe757d60e8f756acee59a
JuanSant <jsantiago@itexico.net>
2017-04-03 14:00:40 -0500

    Login and AuthControllers

--- YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
+++ YDKJS series/3 this and Object Prototypes/ch6_Behavior_Delegation.js
@@ -191,3 +191,65 @@ $( document ).ready( function(){
 //of a web page, and another for actually handling the authentication (communication) with the server.
 //Also is needed a utility helper for making the Ajax communication to the server.
 
+var LoginController = {
+    errors: [],
+    getUser: function(){
+        return document.getElementById("login_username").value;
+    },
+    getPassword: function(){
+        return document.getElementById("login_password").value;
+    },
+    validateEntry: function(user,pw){
+        user = user || this.getUser();
+        pw = pw || this.getPassword();
+        
+        if(!(user && pw)){
+            return this.failure("Please enter a username && password!");
+        }
+        else if (pw.length < 5){
+            return this.failure("Password must be 5+ characters!")
+        }
+        
+        //validated
+        return true;
+    },
+    showDialog: function(title, msg){
+        //display success message to user in dialog
+    },
+    failure: function(err){
+        this.errors.push( err );
+        this.showDialog("Error", "Login invalid" + err)
+    }
+};
+
+var AuthController = Object.create( LoginController );
+
+AuthController.errors: [];
+AuthController.checkAuth = function(){
+    var user = this.getUser();
+    var pw = this.getPassword();
+    
+    if(this.validateEntry(user, pw)){
+        this.server( "/check-auth", {
+          user : user,
+          pw : pw
+        })
+        .then( this.accepted.bind( this ) )
+        .fail( this.rejected.bind( this ) );
+    }
+};
+
+AuthController.server = function(url, data){
+   return $.ajax({
+        url: url,
+        data: data
+   });  
+};
+
+AuthController.accepted = function(){
+    this.showDialog("Success", "Authenticated!")
+};
+
+AuthController.rejected = function(err){
+    this.failure("Auth Failed: " + err)
+};
